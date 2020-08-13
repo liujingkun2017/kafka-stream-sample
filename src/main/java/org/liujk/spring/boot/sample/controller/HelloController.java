207ed44d6ccb7a809388658db65fed591a43ea8b
mac <13164432553@163.com>
2020-08-13 17:07:22 +0800

    fix：update

--- src/main/java/org/liujk/spring/boot/sample/controller/HelloController.java
+++ src/main/java/org/liujk/spring/boot/sample/controller/HelloController.java
@@ -1,18 +1,37 @@
 package org.liujk.spring.boot.sample.controller;
 
+import cn.hutool.json.JSONUtil;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KGroupedStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.liujk.spring.boot.sample.common.Constants;
 import org.liujk.spring.boot.sample.dto.TxSampleDTO;
+import org.liujk.spring.boot.sample.dto.TxSampleStreamDTO;
 import org.liujk.spring.boot.sample.mq.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Random;
 
 @RestController
 public class HelloController {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServers;
 
     @Autowired
     private Publisher publisher;
@@ -24,6 +43,8 @@ public class HelloController {
 
     @RequestMapping("/publish")
     public String publish() {
+
+        KafkaStreams kafkaStreams = start();
 
         for (int i = 0; i < 2000; i++) {
             TxSampleDTO txSampleDTO = new TxSampleDTO();
@@ -39,7 +60,53 @@ public class HelloController {
                 e.printStackTrace();
             }
         }
+
+        stop(kafkaStreams);
+
         return "ok!";
+    }
+
+    private KafkaStreams start() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, Constants.APPLICATION_ID);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KGroupedStream stream = builder.<String, String>stream(Constants.KAFKA_SOURCE_TOPIC).groupByKey();
+
+        KTable aggregate = stream
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(1)).advanceBy(Duration.ofSeconds(1)))
+                .aggregate(
+                        () -> null,
+                        (aggKey, newValue, aggValue) -> {
+
+                            if (StringUtils.isEmpty(aggValue)) {
+                                TxSampleStreamDTO txSampleStreamDTO = JSONUtil.toBean((String) newValue, TxSampleStreamDTO.class);
+                                txSampleStreamDTO.setCount(1);
+                                return JSONUtil.toJsonStr(txSampleStreamDTO);
+                            }
+
+                            TxSampleStreamDTO txSampleStreamDTO = JSONUtil.toBean((String) aggValue, TxSampleStreamDTO.class);
+                            txSampleStreamDTO.setCount(txSampleStreamDTO.getCount() + 1);
+                            return JSONUtil.toJsonStr(txSampleStreamDTO);
+                        },
+                        Materialized.<String, String, KeyValueStore<String, String>>as(Constants.APPLICATION_ID + "-action")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(Serdes.String())
+                );
+        aggregate.toStream().to(Constants.KAFKA_TARGET_TOPIC);
+        Topology topology = builder.build();
+        KafkaStreams streams = new KafkaStreams(topology, props);
+        streams.start();
+        return streams;
+    }
+
+
+    private void stop(KafkaStreams streams) {
+        streams.close();
     }
 
 }
